623811d5d460ec0e5fc23daa783411e1d9437edd
Juan <juan.ochoad@upb.edu.co>
2021-04-20 19:22:57 -0500

    add 'our services', navbar edit, remove another sections of the template

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var boton_flotante = document.getElementById("boton_flotante");
+	boton_flotante.addEventListener("click",function(){
+		var url="https://wa.me/+573022404650";
+		window.open(url,'_blank').focus();
+});
+}
