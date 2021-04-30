34c6e6db9c14ae93e813136222aaceab8e8602d3
Daniel AC <arboleda.daniel785@gmail.com>
2021-04-29 21:54:58 -0500

    Correciones login  y registro

--- js/login.js
+++ js/login.js
@@ -1,21 +1,13 @@
-window.onload = inicio;
 function inicio(){
-    const formloginUI = document.querySelector('#forming');
+    const dataUser = JSON.parse(localStorage.getItem("registro"))
+    const correo = document.getElementById("correo").value
+    const password = document.getElementById("password").value
 
-    formloginUI.addEventListener('submit',(e) =>{
-        e.preventDefault();
-        var validacion = localStorage.getItem(localStorage.key(0));
-        let userUI = document.querySelector('#user').value;
-        let pwUI = document.querySelector('#password').value;
-        if(userUI.length == 0 || pwUI.length == 0){
-            window.alert("Por favor, digite las credenciales");
-        }
-        else if(validacion.includes(userUI) && validacion.includes(pwUI)){
-            formloginUI.reset(); 
-            window.open("galeria.html", "_self");  
-        }
-        else{
-            window.alert("Credenciales incorrectas!");
-        }
-    });
+    if(correo == dataUser.email){
+        password == dataUser.password ? redirect() : alert("contrasena incorrecta")
+    }else alert("correo no registrado")
+}
+const redirect = () => {
+    window.open("./../html/api.html", '_self')
+    /* window.history.push("api.html") */
 }
--- js/registro.js
+++ js/registro.js
@@ -1,82 +1,92 @@
 //Funcion onload
-window.onload = inicio;
-        
-function inicio(){
-    const formregisterUI = document.querySelector('#form-ing');
-    let arrayRegistro = [];
 
-    const CrearUsuario = (user, pw, pwrp, email, number) => {
-        let item = {
-            user: user,
-            pw:pw,
-            pwrp:pwrp,
-            email:email,
-            number:number
-        }   
-        arrayRegistro.push(item);
-        return item;
+
+function inicio() {
+    let formregisterUI = document.getElementById("toast-message")
+    let isPasswordAllowed = true;
+    let userInput = document.getElementById("user").value
+    let emailInput = document.getElementById("email").value
+    let passwordInput = document.getElementById("password").value
+    let passwordRepeatInput = document.getElementById("password-repeat").value
+
+    const userToRegister = {
+        username: userInput,
+        email: emailInput,
+        password: passwordInput,
+        passwordRepeat: passwordRepeatInput,
     }
-    const GuardarDB = () =>{
-        localStorage.setItem('registro',JSON.stringify(arrayRegistro));
+
+    isPasswordAllowed = validar_pw(userToRegister.password)
+
+    if (isPasswordAllowed) {
+        localStorage.setItem('registro', JSON.stringify(userToRegister));
     }
-function validar_pw(pwUI){
-    //validacion pw
-    var mays = false;
-    var minus = false;
-    var numr = false;
-    var symb = false;
-    if(pwUI.length >= 8){
-        for(var i = 0; i <pwUI.length;i++){
-            if(pwUI.charCodeAt(i) >= 65 && pwUI.charCodeAt(i) <= 90){
-                mays = true;
+/* 
+    isPasswordAllowed ? userToRegister.username ? userToRegister.email ? userToRegister.passwordRepeat ? localStorage.setItem('registro', JSON.stringify(userToRegister)) : null : null : null : null
+
+    isPasswordAllowed === "puto" ? (true) : (false) */
+
+
+
+    function validar_pw(pwUI) {
+        //validacion pw
+        var mays = false;
+        var minus = false;
+        var numr = false;
+        var symb = false;
+        if (pwUI.length >= 8) {
+            for (var i = 0; i < pwUI.length; i++) {
+                if (pwUI.charCodeAt(i) >= 65 && pwUI.charCodeAt(i) <= 90) {
+                    mays = true;
+                }
+                else if (pwUI.charCodeAt(i) >= 97 && pwUI.charCodeAt(i) <= 122) {
+                    minus = true;
+                }
+                else if (pwUI.charCodeAt(i) >= 48 && pwUI.charCodeAt(i) <= 57) {
+                    numr = true;
+                }
+                else {
+                    symb = true;
+                }
             }
-            else if(pwUI.charCodeAt(i) >= 97 && pwUI.charCodeAt(i) <= 122){
-                minus = true;
-            }
-            else if(pwUI.charCodeAt(i) >= 48 && pwUI.charCodeAt(i) <= 57){
-                numr = true;
-            }
-            else{
-                symb = true;
+            if (mays == true && minus == true && numr == true && symb == true) {
+                return true;
             }
         }
-        if(mays == true && minus == true && numr == true && symb == true){
-            return true;
-        }
+        return false;
+        //validacion pw
     }
-    return false;
-    //validacion pw
-}
 
 
-    formregisterUI.addEventListener('submit',(e) =>{
+    /* formregisterUI.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let userUI = document.querySelector('#user').value;
         let pwUI = document.querySelector('#password').value;
         let pwrpUI = document.querySelector('#password-repeat').value;
         let emailUI = document.querySelector('#email').value;
-        let numberUI = document.querySelector('#number').value;
 
 
-        if(userUI.length == 0 || pwUI.length == 0 || pwrpUI.length == 0 || emailUI.length == 0 || isNaN(numberUI)) 
-        {
+
+
+
+        if (userUI.length == 0 || pwUI.length == 0 || pwrpUI.length == 0 || emailUI.length == 0) {
             window.alert("Todos los caracteres son obligatorios!");
-            formregisterUI.reset(); 
+            formregisterUI.reset();
         }
-        else if(pwUI != pwrpUI){
+        else if (pwUI != pwrpUI) {
             window.alert("Las contraseñas deben coincidir");
         }
-        else if(validar_pw(pwUI) == false){
-            window.alert("Contraseña debe tener por lo menos 8 caracteres, 1 mayuscula, 1 minuscula, 1 simbolo, 1 número")   
+        else if (validar_pw(pwUI) == false) {
+            window.alert("Contraseña debe tener por lo menos 8 caracteres, 1 mayuscula, 1 minuscula, 1 simbolo, 1 número")
         }
-        else{
-            CrearUsuario(userUI, pwUI, pwrpUI, emailUI,numberUI);
+        else {
+            CrearUsuario(userUI, pwUI, pwrpUI, emailUI);
             GuardarDB();
-            formregisterUI.reset(); 
-            window.open("login.html", "_self");     
+            formregisterUI.reset();
+            window.open("login.html", "_self");
         }
-    });
+    }); */
 }
 
 
